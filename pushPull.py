17020a0f28ea52e1b99bb4f178205629c05780e2
luck-i <ansas4565@gmail.com>
2022-08-20 09:46:06 +0300

    20/08/2022-09:46:05

---
+++ pushPull.py
@@ -0,0 +1,27 @@
+import os 
+from datetime import datetime
+
+now = datetime.now()   
+dt_string = now.strftime("%d/%m/%Y-%H:%M:%S")
+
+def push():
+    os.system('git add .')
+    os.system(f"git commit -m {dt_string}")
+    os.system('git push')
+    print('push done')
+def pull():
+    os.system('git pull')
+    print('pull done')
+def status():
+    os.system('git status')
+    print('status done')
+def log():
+    os.system('git log')
+    print('log done')
+def branch():
+    os.system('git branch')
+    print('branch done')
+
+push()
+
+# something
